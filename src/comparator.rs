7a78a39882c1f1313626e246ee394e8d5a70ead1
Bernát Süli <suli.bernat@gmail.com>
2025-11-07 17:52:59 +0100

    proper PWM/comparator implementation

---
+++ src/comparator.rs
@@ -0,0 +1,137 @@
+/*
+    There is no support for comparators in Embassy (and neither is it in the rust embeded HALs),
+    so it has to be directly configured by writing to the registers.
+
+    It's status can be check with pooling or interrupts.
+    For interrrupts it uses EXTI Line 17 and 18.
+*/
+
+use defmt::info;
+use embassy_stm32::interrupt;
+
+/// Comparator 1 control and status register
+const COMP1_CSR: *mut u32 = 0x4001_0200u32 as *mut u32;
+/// Comparator 2 control and status register
+const COMP2_CSR: *mut u32 = (0x4001_0200u32 + 0x4u32) as *mut u32;
+
+// NVIC ADC_COMP: ADC and COMP interrupts (ADC combined with EXTI lines 17 & 18)
+/// NVIC BASE ADDRESS:
+const NVIC_ISER0: *mut u32 = 0xE000_E100u32 as *mut u32;
+const NVIC_ADC_COMP: u32 = 0x1u32 << 12;
+const NVIC_ICPR0: *mut u32 = 0xE000E280u32 as *mut u32;
+
+const COMP1_EXTI_LINE: u32 = 1u32 << 17;
+const COMP2_EXTI_LINE: u32 = 1u32 << 18;
+
+/// EXTI base address:
+const EXTI: u32 = 0x4002_1800u32;
+/// EXTI rising trigger selection register
+const EXTI_RTSR1: *mut u32 = EXTI as *mut u32;
+/// EXTI falling trigger selection register 1
+const EXTI_FTSR1: *mut u32 = (EXTI + 0x004u32) as *mut u32;
+/// EXTI rising edge pending register 1
+const EXTI_RPR1: *mut u32 = (EXTI + 0x00Cu32) as *mut u32;
+/// EXTI falling edge pending register 1
+const EXTI_FPR1: *mut u32 = (EXTI + 0x010u32) as *mut u32;
+/// EXTI CPU wake-up with interrupt mask register
+const EXTI_IMR1: *mut u32 = (EXTI + 0x080u32) as *mut u32;
+/// EXTI CPU wake-up with event mask register
+const EXTI_EMR1: *mut u32 = (EXTI + 0x084u32) as *mut u32;
+
+/*
+    Dirty hack:
+    The following registry value works, but it overwrites multiple reserved bits, whose values we where supposed to keep
+    0b0000_0000_0000_0010_0000_0010_0010_0001
+    - 0000_000 -> bit 31-30 is status reg (LOCK and VALUE), can't write. Next 5 are reserved.
+    - BLANKSEL: 0_0000 - no blanking
+    - PWRMODE:  00   - high speed
+    - HYST:     10   - medium hysteresis
+    - polarity: 0    - non-inverted
+    - WINOUT:   0    - window mode disabled
+    - WINMODE:  000  - noninverting input selector for window mode (not used)
+    - INPSEL:   010  - input pin selection: PA1 and PA3
+    - INMSEL:   0010 - 3/4 Vref,int
+    - enabled:  0001
+
+    The same value works for both COMPs.
+*/
+
+/// Comparator control and status register default
+const CSR_DEFAULT: u32 = 0b0000_0000_0000_0010_0000_0010_0010_0001;
+
+/// Setting both of the comparator status registers to the CSR_DEFAULT value
+pub fn set_default_csr() {
+    unsafe {
+        core::ptr::write_volatile(COMP1_CSR, CSR_DEFAULT);
+        //core::ptr::write_volatile(COMP2_CSR, CSR_DEFAULT);
+    }
+}
+
+/// Sets EXTI_RTSR1 bit 17 & 18 to 1. Enabling rising edge interrupts for COMP 1 & 2;
+pub fn enable_interrupts() {
+    unsafe {
+        let rtsr = core::ptr::read_volatile(EXTI_RTSR1) | COMP1_EXTI_LINE;
+        core::ptr::write_volatile(EXTI_RTSR1, rtsr); // set rising edge detection
+        core::ptr::write_volatile(EXTI_RPR1, COMP1_EXTI_LINE); // clear pending register
+
+        core::ptr::write_volatile(EXTI_FTSR1, rtsr); // set falling edge detection
+        core::ptr::write_volatile(EXTI_FPR1, COMP1_EXTI_LINE); // clear pending register
+
+        let emr = core::ptr::read_volatile(EXTI_EMR1) & !COMP1_EXTI_LINE;
+        core::ptr::write_volatile(EXTI_EMR1, emr); // disable even mode
+
+        let imr = core::ptr::read_volatile(EXTI_IMR1) | COMP1_EXTI_LINE;
+        core::ptr::write_volatile(EXTI_IMR1, imr); // enable interrupt mode
+
+        core::ptr::write_volatile(NVIC_ICPR0, NVIC_ADC_COMP); // clear any pending interrupts
+        core::ptr::write_volatile(NVIC_ISER0, NVIC_ADC_COMP); // enable NVIC interrupt
+
+        info!("COMP interrupts set!");
+    }
+}
+
+// bit number 30 is the comparator output, 1 - high, 0 - low
+fn read_comp_status(register: u32) -> bool {
+    register & (1 << 30) != 0
+}
+
+/// Returns the output level of the comparator.
+/// Low -> false, High -> true
+pub fn read_comp1_value() -> bool {
+    let csr = unsafe { core::ptr::read_volatile(COMP1_CSR) };
+
+    read_comp_status(csr)
+}
+
+/// Returns the output level of the comparator.
+/// Low -> false, High -> true
+pub fn read_comp2_value() -> bool {
+    let csr = unsafe { core::ptr::read_volatile(COMP2_CSR) };
+
+    read_comp_status(csr)
+}
+
+/// TIM2 capture/compare enable register (TIM2_CCER)
+/// 0x4000 0000 + 0x20
+const TIM2_CCER: *mut u32 = 0x4000_0020u32 as *mut u32;
+const CH4_ENABLE: u32 = 0x1u32 << 12;
+
+#[interrupt]
+unsafe fn ADC_COMP1_2() {
+    let comp1 = read_comp1_value();
+    unsafe {
+        let ccer = core::ptr::read_volatile(TIM2_CCER);
+        if comp1 {
+            core::ptr::write_volatile(TIM2_CCER, ccer & !CH4_ENABLE); // disables PWM output
+            //info!("PWM off");
+        } else {
+            core::ptr::write_volatile(TIM2_CCER, ccer | CH4_ENABLE); // enables PWM output
+            //info!("PWM on");
+        }
+    }
+    // reset EXTI_RPR1
+    unsafe {
+        core::ptr::write_volatile(EXTI_RPR1, COMP1_EXTI_LINE);
+        core::ptr::write_volatile(NVIC_ICPR0, NVIC_ADC_COMP);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ mod mc_24cs256;
 mod sht40;
 mod stc3315;
 mod usb_connection;
+mod comparator;
 
 type I2c1Bus = Mutex<NoopRawMutex, I2c<'static, Blocking, i2c::Master>>;
 type RtcShared = Mutex<NoopRawMutex, Rtc>;
@@ -76,6 +77,7 @@ async fn main(spawner: Spawner) {
 
     let p = embassy_stm32::init(system_config);
 
+
     // Shared I2C bus for tasks
     // Default is 100kHz, medium speed GPIO, no internal pull-ups
     let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, i2c::Config::default());
@@ -84,29 +86,17 @@ async fn main(spawner: Spawner) {
 
     /*
         HV PSU:
-            - comp1:
-                + PA1
-                - Vref
-                0x4001_0200 - 0x4001_03FF COMP reg address
-                COMP1_CSR: Comparator 1 control and status register, Address offset: 0x00
-                     0000_0000_0000_0101 - medium speed, low hyst(??)
-                     0000_0[010]_[0001]_0001 - INPSEL - INMSEL (Vref /2 )
-            - comp2:
-                + PA3
-                - Vref
-                COMP2_CSR: Same as COMP1_CSR, Address offset: 0x04
-            - pwm_out: PB11
     */
 
-    // There is no support for comparators in Embassy yet(??), so we do this the hard way
-    unsafe {
-        const COMP1_CSR: *mut u32 = 0x4001_0200 as *mut u32;
-        //const COMP2_CSR: *mut u32 = (0x4001_0200 + 0x04) as *mut u32;
-        core::ptr::write_volatile(COMP1_CSR, 0b0000_0000_0000_0101_0000_0010_0001_0001);
-    }
+    // Setting up the comparators
+    comparator::enable_interrupts();
+    comparator::set_default_csr();
+    // enable interrupts globally
+    unsafe { cortex_m::interrupt::enable(); }
 
+    // PWM gen
     let tim2_ch4 = PwmPin::new(p.PB11, embassy_stm32::gpio::OutputType::PushPull);
-    let pwm = SimplePwm::new(
+    let mut pwm_controler = SimplePwm::new(
         p.TIM2,
         None,
         None,
@@ -115,7 +105,9 @@ async fn main(spawner: Spawner) {
         khz(10),
         Default::default(),
     );
-    spawner.spawn(comparator_1(pwm)).unwrap();
+    let mut pwm = pwm_controler.ch4();
+    pwm.set_duty_cycle_percent(20);
+    pwm.enable();
 
     /*
         pulse_in (PC13) --> pulse_counter
@@ -314,62 +306,6 @@ async fn user_button_fn(mut button: ExtiInput<'static>) {
     }
 }
 
-// bit number 30 is the comparator output, 1 - high, 0 - low
-fn read_comp_status(register: u32) -> bool {
-    register & (1 << 30) != 0
-}
-/*
-    Comparator status checking
-    currently it uses pooling to achive this,
-    but probably want to use interrupts.
-
-    HV_PSU_ENABLE enables/disables the PSU regardles of the comp values.
-    While developping, it starts of, and manually has to be turned on.
-*/
-#[embassy_executor::task]
-async fn comparator_1(mut pwm: SimplePwm<'static, peripherals::TIM2>) {
-    const COMP1_CSR: *mut u32 = 0x4001_0200 as *mut u32;
-    let mut comp1_status_old = unsafe { read_comp_status(core::ptr::read_volatile(COMP1_CSR)) };
-
-    let mut active_ch = pwm.ch4();
-    active_ch.set_duty_cycle_percent(10); // placeholder
-
-    let mut psu_enable_buf = HV_PSU_ENABLE.load(Ordering::Relaxed);
-
-    loop {
-        if HV_PSU_ENABLE.load(Ordering::Relaxed) {
-            let comp1_status_new = unsafe { read_comp_status(core::ptr::read_volatile(COMP1_CSR)) };
-
-            if comp1_status_new != comp1_status_old {
-                comp1_status_old = comp1_status_new;
-
-                if comp1_status_new {
-                    active_ch.disable();
-                } else {
-                    active_ch.enable();
-                }
-            }
-
-            if !psu_enable_buf {
-                psu_enable_buf = true;
-                info!("HV PSU: enabled by user");
-                if !comp1_status_new {
-                    active_ch.enable();
-                }
-            }
-
-            Timer::after_micros(100).await;
-        } else {
-            if psu_enable_buf {
-                psu_enable_buf = false;
-                info!("HV PSU: disabled by user");
-                active_ch.disable();
-            }
-
-            Timer::after_secs(1).await;
-        }
-    }
-}
 
 // RTC "alarm", uses pooling as proper alarms are not supported (?) yet
 // Set to trigger every min while testing. Final product should trigger every hour
